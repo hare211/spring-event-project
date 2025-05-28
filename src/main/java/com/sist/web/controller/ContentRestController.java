471c3ffdf99de6b22d195c9b25606390fb736ac6
hare211 <blackhole2748@gmail.com>
2025-05-28 16:25:21 +0900

    RestController CrossOrigin 추가

--- src/main/java/com/sist/web/controller/ContentRestController.java
+++ src/main/java/com/sist/web/controller/ContentRestController.java
@@ -3,6 +3,7 @@ package com.sist.web.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.sist.web.service.ContentService;
 
 import lombok.RequiredArgsConstructor;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/content")
